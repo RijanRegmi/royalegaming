bb1d19a6055817bfecd5bf964c77eb6f6b1bde73
Rijan Regmi <rijanregmi111@gmail.com>
2026-05-25 20:38:42 +0545

    fix notification

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -16,9 +16,13 @@ if (!admin.apps.length) {
       // 2. Fall back to env variables
       const projectId = process.env.FIREBASE_PROJECT_ID;
       const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
-      const privateKey = process.env.FIREBASE_PRIVATE_KEY
-        ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
-        : undefined;
+      let privateKey = process.env.FIREBASE_PRIVATE_KEY;
+      if (privateKey) {
+        if (privateKey.startsWith('"') && privateKey.endsWith('"')) {
+          privateKey = privateKey.substring(1, privateKey.length - 1);
+        }
+        privateKey = privateKey.replace(/\\n/g, '\n');
+      }
 
       if (projectId && clientEmail && privateKey) {
         admin.initializeApp({
